1e1329a6020a32df52b6d1266f7ed6a24eac8b8c
hwcall1102 <haydenc0293@gmail.com>
2025-04-07 23:47:04 -0600

    html and css basic

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,17 +22,6 @@ export function getParam(param) {
   return product;
 }
 
-// function to take a list of objects and a template and insert the objects as HTML into the DOM
-
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
-  const htmlStrings = list.map(templateFn);
-  // if clear is true we need to clear out the contents of the parent.
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
-
 export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.innerHTML = template;
   if(callback) {
@@ -55,41 +44,4 @@ export async function loadHeaderFooter() {
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
-}
-
-// set a listener for both touchend and click
-export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  qs(selector).addEventListener("click", callback);
-}
-
-export function alertMessage(message, scroll = true) {
-  // create element to hold the alert
-  const alert = document.createElement('div');
-  // add a class to style the alert
-  alert.classList.add('alert');
-  alert.innerHTML = `<p>${message}</p><span>X</span>`;
-
-  // add a listener to the alert to see if they clicked on the X
-  // if they did then remove the child
-  alert.addEventListener('click', function(e) {
-      if(e.target.tagName == "SPAN") { // how can you tell if they clicked on the X or on something else?  hint: check out e.target.tagName or e.target.innerText
-        main.removeChild(this);
-      }
-  })
-  // add the alert to the top of main
-  const main = document.querySelector('main');
-  main.prepend(alert);
-  // make sure they see the alert by scrolling to the top of the window
-  // you may not always want to do this...so default to scroll=true, but allow it to be passed in and overridden.
-  if(scroll)
-    window.scrollTo(0,0);
-}
-
-export function removeAllAlerts() {
-  const alerts = document.querySelectorAll(".alert");
-  alerts.forEach((alert) => document.querySelector("main").removeChild(alert));
 }
